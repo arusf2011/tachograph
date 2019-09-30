bc0bf98e5c3fc7313827b8ee119c9ee2d4237304
arusf2011 <afatyga861@gmail.com>
2019-10-01 00:37:50 +0200

    Fixed issue #1 reported by bensherred

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 $f3 = require('./lib/base.php');
 $f3->set('LOCALES','./app/dict/');
 $f3->set('FALLBACK','en');
-require_once './app/functions.php';
+if(file_exists('./LOCK'))
+{
+    require_once './app/functions.php';
+}
+else
+{
+    echo null;
+}
 require_once './router.php';
 $f3->run();
