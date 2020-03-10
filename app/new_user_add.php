2fc83dbe292fa6f442a7f94075c6af332621dfe6
arusf2011 <afatyga861@gmail.com>
2020-03-10 21:54:53 +0100

    Version 1.1.2

--- app/new_user_add.php
+++ app/new_user_add.php
@@ -10,6 +10,8 @@
     $f3->set('user_data',$db->exec('SELECT * FROM users WHERE nickname = ?',$_SESSION['nickname']));
     $user_data = $f3->get('user_data');
     $f3->set('roles_arr',$db->exec('SELECT * FROM roles'));
+    $f3->set('global_settings',$db->exec('SELECT * FROM settings'));
+    $global_settings = $f3->get('global_settings');
     $roles = $f3->get('roles_arr');
     $role = $user_data[0]['role_id'];
     if($roles[$role-1]['admin'] == false)
